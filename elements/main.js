77833075d5b2aa07132f024c38b81cdc8ba413be
Naimur53 <naimurrhman53@gmail.com>
2021-06-25 21:43:14 +0600

    done

---
+++ elements/main.js
@@ -0,0 +1,19 @@
+function createSnow(){
+    const snowFlake = document.createElement("i");
+
+    snowFlake.classList.add("far");
+    snowFlake.classList.add("fa-snowflake");
+
+    snowFlake.style.left = Math.random()*window.innerWidth +"px";
+    snowFlake.style.animationDuration = Math.random() * 5 + 3 +"s";
+
+    snowFlake.style.opacity = Math.random();
+    snowFlake.style.fontSize = Math.random()*10 + 10 +"px";
+
+
+    document.body.appendChild(snowFlake);
+ 
+
+} 
+setInterval(createSnow,100);
+
