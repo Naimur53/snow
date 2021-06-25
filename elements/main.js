ef78f6cf8e14af38b8617877f8119dd8fddff7e1
Naimur53 <naimurrhman53@gmail.com>
2021-06-25 21:54:34 +0600

    doen

--- elements/main.js
+++ elements/main.js
@@ -8,9 +8,8 @@ function createSnow(){
     snowFlake.style.animationDuration = Math.random() * 5 + 3 +"s";
 
     snowFlake.style.opacity = Math.random();
-    snowFlake.style.fontSize = Math.random()*10 + 10 +"px";
-
-
+    snowFlake.style.fontSize = Math.random()*10 + 10 +"px"; 
+    
     document.body.appendChild(snowFlake);
  
 
